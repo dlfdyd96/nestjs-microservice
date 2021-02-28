51a240860b85d286b0802d6f843869663047d824
dlfdyd96 <1yongs_@naver.com>
2021-02-28 23:44:06 +0900

    Auth: Connect Microservice Main

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: 'localhost',
+      port: 4000,
+    },
+  });
+
+  await app.startAllMicroservices();
   await app.listen(process.env.NODE_PORT);
 }
 bootstrap();
